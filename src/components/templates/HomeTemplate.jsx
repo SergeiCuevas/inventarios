976c31f98f0f41be076dc9062b3d0ee502dfaddf
SergioAndroide <sergeicuevas1@gmail.com>
2024-02-07 14:01:53 -0600

    rutas y home

---
+++ src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export function HomeTemplate(){
+    return (
+    
+    <Container>
+        <h1>Qué pedo</h1>
+    </Container> );
+}
+
+const Container =styled.div 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as App } from './App';
+export { AuthContextProvider } from './context/authcontext';
+export {  } from './main';
+export { supabase } from './supabase/supabase.config';
+export { DataModulosConfiguracion, DesplegableUser, LinksArray, SecondarylinksArray, TemasData, TipoDocData, TipouserData } from './utils/dataEstatica';
